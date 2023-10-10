8ab975e785e8cc3339068030a6978dd25f40f3a4
Ansh Jaiswal <jaiansh2016@gmail.com>
2023-10-10 19:48:53 +0530

    speed slowed

--- Assets/scripts/pipes.cs
+++ Assets/scripts/pipes.cs
@@ -5,7 +5,7 @@ public class Pipes : MonoBehaviour
     public Transform top;
     public Transform bottom;
 
-    public float speed = 5f;
+    public float speed = 3.5f;
     private float leftEdge;
 
     private void Start()
